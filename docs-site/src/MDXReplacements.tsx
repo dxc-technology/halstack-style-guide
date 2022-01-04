24e5a004469500cc9bad39c06e705e6fde637db1
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-01-04 18:32:02 +0100

    Add wrapper, mg, p and ul replacements

--- docs-site/src/MDXReplacements.tsx
+++ docs-site/src/MDXReplacements.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { DxcHeading } from "@dxc-technology/halstack-react";
 import slugify from "slugify";
 import HeadingLink from "./HeadingLink";
+import styled from "styled-components";
 
 export const h1 = ({ children }) => (
   <DxcHeading level={2} as="h1" text={children} />
@@ -23,3 +24,50 @@ export const h4 = ({ children }) => (
 export const h5 = ({ children }) => (
   <DxcHeading level={5} as="h5" text={children} />
 );
+
+export const wrapper = ({ children }) => (
+  <Wrapper>{ children }</Wrapper>
+);
+
+export const img = (props) => (
+  <Img { ...props }></Img>
+);
+
+export const p = ({ children }) => (
+  <P>{ children }</P>
+);
+
+export const ul = ({ children }) => (
+  <Ul>{ children }</Ul>
+);
+
+
+
+
+function contentWrapper() {
+  return (
+    <Wrapper></Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  max-width: 80ch;
+`;
+
+const Img = styled.img`
+width: 100%;
+`;
+
+const P = styled.p`
+line-height: 1.5em;
+`;
+
+const Ul = styled.ul`
+& li { line-height: 1.5em;}
+& li:not(:first-child){
+  margin-top:0.5rem
+}
+
+`;
+
+
