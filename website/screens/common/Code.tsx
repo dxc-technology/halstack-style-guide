81d2c676980bcc567df3e99d808bc742f68a5b65
Jesús Suárez Ardid <jesussuarezardid@gmail.com>
2022-02-24 16:33:09 +0100

    Fix code component

--- website/screens/common/Code.tsx
+++ website/screens/common/Code.tsx
@@ -4,5 +4,7 @@ const StyledCode = styled.code`
   background-color: #f2eafa;
   color: #5f249f;
   font-size: inherit;
+  padding: 2px 4px;
+  border-radius: 0.25rem;
 `;
 export default StyledCode;
