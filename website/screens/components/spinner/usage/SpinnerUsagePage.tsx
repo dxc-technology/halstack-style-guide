c70e47da73c6fbfaf2e112f5c582493348fa8e0f
agonzalez97 <aidamaria.gonzalez@dxc.com>
2022-03-01 14:12:07 +0100

    Doc page for Spinner component

---
+++ website/screens/components/spinner/usage/SpinnerUsagePage.tsx
@@ -0,0 +1,71 @@
+import { DxcText, DxcList, DxcStack } from "@dxc-technology/halstack-react";
+import Image from "next/image";
+import HeadingLink from "../../../common/HeadingLink";
+import variantsImage from "./images/spinner_variants.png";
+import Figure from "../../../common/Figure";
+import Code from "../../../common/Code";
+import determinateImage from "./images/spinner_determinate_indeterminate.png";
+import DocFooter from "../../../common/DocFooter";
+
+const SpinnerUsagePage = () => {
+  return (
+    <DxcStack gutter="xxxlarge">
+      <DxcStack gutter="large">
+        <HeadingLink level={2}>Usage</HeadingLink>
+        <DxcList>
+          <DxcText as="p">
+            There should only be a single spinner on a page at one time.
+          </DxcText>
+          <DxcText as="p">
+            Only use the spinner component in a process that takes more than one
+            second.
+          </DxcText>
+          <DxcText as="p">
+            The text of the action it is not mandatory but recommendable.
+          </DxcText>
+          <DxcText as="p">
+            If only a portion of a page is displaying new content or being
+            updated, use a <Code>medium</Code> or <Code>small</Code> spinner
+            place the spinner in that part of the page.
+          </DxcText>
+        </DxcList>
+        <HeadingLink level={2}>Variants</HeadingLink>
+        <DxcText as="p">
+          There are three different variants for the spinner component due to
+          the size or the position: <b>large</b>, <b>small</b> and{" "}
+          <b>overlay</b>.
+        </DxcText>
+        <Figure caption="Spinner component variants">
+          <Image src={variantsImage} alt="Spinner component variants" />
+        </Figure>
+        <HeadingLink level={2}>Determinate or indeterminate</HeadingLink>
+        <Figure caption="Determinate and indeterminate spinners">
+          <Image
+            src={determinateImage}
+            alt="Determinate and indeterminate spinners"
+          />
+        </Figure>
+        <DxcList>
+          <DxcText as="p">
+            Determinate indicators display how long a process will take. They
+            should be used in longer processes.
+          </DxcText>
+          <DxcText as="p">
+            Indeterminate indicators express an unspecified amount of wait time.
+            They should be used when:
+            <DxcList>
+              <DxcText as="p">The processing time is unknown.</DxcText>
+              <DxcText as="p">
+                The wait time is expected to be short enough that it’s not
+                necessary to display.
+              </DxcText>
+            </DxcList>
+          </DxcText>
+        </DxcList>
+      </DxcStack>
+      <DocFooter githubLink="https://github.com/dxc-technology/halstack-style-guide/blob/master/website/screens/components/spinner/usage/SpinnerUsagePage.tsx" />
+    </DxcStack>
+  );
+};
+
+export default SpinnerUsagePage;
