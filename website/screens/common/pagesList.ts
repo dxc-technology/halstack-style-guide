f183fd4e1f628937621c8be4d119a3ffd21aae41
marcialfps <m.parrilla@dxc.com>
2022-03-07 12:14:39 +0100

    Doc page for Footer component

--- website/screens/common/pagesList.ts
+++ website/screens/common/pagesList.ts
@@ -20,6 +20,7 @@ const overviewLinks: LinkDetails[] = [
 const componentsLinks: LinkDetails[] = [
   { label: "Button", path: "/components/button" },
   { label: "Card", path: "/components/card" },
+  { label: "Footer", path: "/components/footer" },
   { label: "Spinner", path: "/components/spinner" },
   { label: "Switch", path: "/components/switch" },
   { label: "Text Input", path: "/components/text-input" },
