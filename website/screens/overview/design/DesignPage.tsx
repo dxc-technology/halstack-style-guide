f5162914ed67b3d7a4af8748e030dfab5b13b910
Alex Bueno <44420072+aweell@users.noreply.github.com>
2022-03-28 14:07:50 +0200

    Update page texts

--- website/screens/overview/design/DesignPage.tsx
+++ website/screens/overview/design/DesignPage.tsx
@@ -2,7 +2,6 @@ import PageLayout from "../../common/PageLayout";
 import {
   DxcHeading,
   DxcText,
-  DxcList,
   DxcStack,
   DxcLink,
 } from "@dxc-technology/halstack-react";
@@ -20,24 +19,27 @@ const Design = () => {
             weight="bold"
           ></DxcHeading>
           <DxcText as="p">
-            Halstack is the DXC Technology&#39;s open source design system for
-            insurance products and digital experiences. Our system provides all
-            the tools and resources needed to create superior, beautiful but
-            above all, functional user experiences.
+            We provide tools for designers that help them to create any application using Halstack design system. 
           </DxcText>
         </DxcStack>
         <DxcStack gutter="large">
+          <HeadingLink level={2}>Design guidance</HeadingLink>
+
+          <DxcText as="p">The design system principles and component usage</DxcText>
+
+
+        </DxcStack>
+        <DxcStack gutter="large">
           <HeadingLink level={2}>Design kits</HeadingLink>
+
+          <DxcText as="p">We have available and Adobe XD public library that is constantly updated with </DxcText>
 
           <DxcLink
               href="https://shared-assets.adobe.com/link/732533f4-d925-487e-4761-9a760574cfac"
               text="Adobe XD public library"
             />{" "}
         </DxcStack>
-        <DxcStack gutter="large">
-          <HeadingLink level={2}>Component usage</HeadingLink>
-
-        </DxcStack>
+        
         <DocFooter githubLink="https://github.com/dxc-technology/halstack-style-guide/blob/master/website/screens/overview/introduction/IntroductionPage.tsx" />
       </DxcStack>
     </PageLayout>
